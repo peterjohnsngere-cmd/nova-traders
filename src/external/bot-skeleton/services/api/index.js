772f96d651b6a83a0866a80439e5becc3d53e160
peterjohnsngere-cmd <peterjohnsngere@gmail.com>
2026-09-15 15:26:23 +0300

    Update index.js

--- src/external/bot-skeleton/services/api/index.js
+++ src/external/bot-skeleton/services/api/index.js
@@ -4,3 +4,4 @@ export { default as NetworkMonitor } from './network_monitor';
 export { default as ServerTime } from './server_time';
 export { default as TradingTimes } from './trading-times';
 export { default as TicksService } from './ticks_service';
+export { api_base } from './api-base';
