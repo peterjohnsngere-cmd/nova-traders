4b4fc35ce201787c993ecd96e702a4a08e3b743f
peterjohnsngere-cmd <peterjohnsngere@gmail.com>
2026-09-17 17:26:22 +0300

    Update index.js

--- src/external/bot-skeleton/services/tradeEngine/trade/state/actions/index.js
+++ src/external/bot-skeleton/services/tradeEngine/trade/state/actions/index.js
@@ -1,42 +1,89 @@
-import * as constants from '../constants';
+ import * as constants from '../constants';
 
 const dispatchIfScopeIs = ({ dispatch, getState, data, scope }) => {
-    const { scope: currentScope } = getState();
-    if (currentScope === scope) {
-        dispatch(data);
-    }
+const { scope: currentScope } = getState();
+
+```
+if (currentScope === scope) {
+    dispatch(data);
+}
+```
+
 };
 
 export const start = () => (dispatch, getState) =>
-    dispatchIfScopeIs({ dispatch, getState, data: { type: constants.START }, scope: constants.STOP });
+dispatchIfScopeIs({
+dispatch,
+getState,
+data: { type: constants.START },
+scope: constants.STOP,
+});
 
-export const proposalsReady = () => ({ type: constants.PROPOSALS_READY });
+export const proposalsReady = () => ({
+type: constants.PROPOSALS_READY,
+});
 
-export const clearProposals = () => ({ type: constants.CLEAR_PROPOSALS });
+export const clearProposals = () => ({
+type: constants.CLEAR_PROPOSALS,
+});
 
-const dispatchIfScopeIsBeforePurchase = args => dispatchIfScopeIs({ ...args, scope: constants.BEFORE_PURCHASE });
+const dispatchIfScopeIsBeforePurchase = args =>
+dispatchIfScopeIs({
+...args,
+scope: constants.BEFORE_PURCHASE,
+});
 
 const dispatchIfBeforePurchaseReady = args => {
-    const { getState } = args;
-    const { proposalsReady: beforePurchaseReady } = getState();
+const { getState } = args;
+const { proposalsReady: beforePurchaseReady } = getState();
 
-    if (beforePurchaseReady) {
-        dispatchIfScopeIsBeforePurchase(args);
-    }
+```
+if (beforePurchaseReady) {
+    dispatchIfScopeIsBeforePurchase(args);
+}
 
-    dispatchIfScopeIs({ ...args, scope: constants.BEFORE_PURCHASE });
+dispatchIfScopeIs({
+    ...args,
+    scope: constants.BEFORE_PURCHASE,
+});
+```
+
 };
 
 export const purchaseSuccessful = () => (dispatch, getState) =>
-    dispatchIfBeforePurchaseReady({ dispatch, getState, data: { type: constants.PURCHASE_SUCCESSFUL } });
+dispatchIfBeforePurchaseReady({
+dispatch,
+getState,
+data: { type: constants.PURCHASE_SUCCESSFUL },
+});
 
 export const openContractReceived = () => (dispatch, getState) => {
-    const { scope: currentScope } = getState();
-    if (currentScope === constants.DURING_PURCHASE) {
-        dispatch({ type: constants.OPEN_CONTRACT });
-    }
-    dispatchIfBeforePurchaseReady({ dispatch, getState, data: { type: constants.OPEN_CONTRACT } });
+const { scope: currentScope } = getState();
+
+```
+if (currentScope === constants.DURING_PURCHASE) {
+    dispatch({
+        type: constants.OPEN_CONTRACT,
+    });
+}
+
+dispatchIfBeforePurchaseReady({
+    dispatch,
+    getState,
+    data: { type: constants.OPEN_CONTRACT },
+});
+```
+
 };
 
 export const sell = () => (dispatch, getState) =>
-    dispatchIfScopeIs({ dispatch, getState, data: { type: constants.SELL }, scope: constants.DURING_PURCHASE });
+dispatchIfScopeIs({
+dispatch,
+getState,
+data: { type: constants.SELL },
+scope: constants.DURING_PURCHASE,
+});
+
+export const tradeAgain = () => ({
+type: constants.TRADE_AGAIN,
+});
