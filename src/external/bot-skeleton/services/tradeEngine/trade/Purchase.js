b6c7a301c1f97e59378cc520d7011490796f6f54
peterjohnsngere-cmd <peterjohnsngere@gmail.com>
2026-09-20 12:44:05 +0300

    Update Purchase.js

--- src/external/bot-skeleton/services/tradeEngine/trade/Purchase.js
+++ src/external/bot-skeleton/services/tradeEngine/trade/Purchase.js
@@ -11,135 +11,111 @@ let purchase_reference;
 export default Engine =>
 class Purchase extends Engine {
 purchase(contract_type) {
+// Prevent calling purchase twice
 if (this.store.getState().scope !== BEFORE_PURCHASE) {
 return Promise.resolve();
 }
 
-```
-        const onSuccess = response => {
-            const { buy } = response;
+const onSuccess = response => {  
+            // Don't unnecessarily send a forget request for a purchased contract.  
+            const { buy } = response;  
 
-            contractStatus({
-                id: 'contract.purchase_received',
-                data: buy.transaction_id,
-                buy,
-            });
+            contractStatus({  
+                id: 'contract.purchase_received',  
+                data: buy.transaction_id,  
+                buy,  
+            });  
 
-            this.contractId = buy.contract_id;
-            this.store.dispatch(purchaseSuccessful());
+            this.contractId = buy.contract_id;  
+            this.store.dispatch(purchaseSuccessful());  
 
-            if (this.is_proposal_subscription_required) {
-                this.renewProposalsOnPurchase();
-            }
+            if (this.is_proposal_subscription_required) {  
+                this.renewProposalsOnPurchase();  
+            }  
 
-            delayIndex = 0;
+            delayIndex = 0;  
+            log(LogTypes.PURCHASE, { transaction_id: buy.transaction_id });  
+            info({  
+                accountID: this.accountInfo.loginid,  
+                totalRuns: this.updateAndReturnTotalRuns(),  
+                transaction_ids: { buy: buy.transaction_id },  
+                contract_type,  
+                buy_price: buy.buy_price,  
+            });  
+        };  
 
-            log(LogTypes.PURCHASE, {
-                transaction_id: buy.transaction_id,
-            });
+        if (this.is_proposal_subscription_required) {  
+            const { id, askPrice } = this.selectProposal(contract_type);  
 
-            info({
-                accountID: this.accountInfo.loginid,
-                totalRuns: this.updateAndReturnTotalRuns(),
-                transaction_ids: {
-                    buy: buy.transaction_id,
-                },
-                contract_type,
-                buy_price: buy.buy_price,
-            });
-        };
+            const action = () => api_base.api.send({ buy: id, price: askPrice });  
 
-        if (this.is_proposal_subscription_required) {
-            const { id, askPrice } = this.selectProposal(contract_type);
+            this.isSold = false;  
 
-            const action = () =>
-                api_base.api.send({
-                    buy: id,
-                    price: askPrice,
-                });
+            contractStatus({  
+                id: 'contract.purchase_sent',  
+                data: askPrice,  
+            });  
 
-            this.isSold = false;
+            if (!this.options.timeMachineEnabled) {  
+                return doUntilDone(action).then(onSuccess);  
+            }  
 
-            contractStatus({
-                id: 'contract.purchase_sent',
-                data: askPrice,
-            });
+            return recoverFromError(  
+                action,  
+                (errorCode, makeDelay) => {  
+                    // if disconnected no need to resubscription (handled by live-api)  
+                    if (errorCode !== 'DisconnectError') {  
+                        this.renewProposalsOnPurchase();  
+                    } else {  
+                        this.clearProposals();  
+                    }  
 
-            if (!this.options.timeMachineEnabled) {
-                return doUntilDone(action).then(onSuccess);
-            }
+                    const unsubscribe = this.store.subscribe(() => {  
+                        const { scope, proposalsReady } = this.store.getState();  
+                        if (scope === BEFORE_PURCHASE && proposalsReady) {  
+                            makeDelay().then(() => this.observer.emit('REVERT', 'before'));  
+                            unsubscribe();  
+                        }  
+                    });  
+                },  
+                ['PriceMoved', 'InvalidContractProposal'],  
+                delayIndex++  
+            ).then(onSuccess);  
+        }  
+        const trade_option = tradeOptionToBuy(contract_type, this.tradeOptions);  
+        const action = () => api_base.api.send(trade_option);  
 
-            return recoverFromError(
-                action,
-                (errorCode, makeDelay) => {
-                    if (errorCode !== 'DisconnectError') {
-                        this.renewProposalsOnPurchase();
-                    } else {
-                        this.clearProposals();
-                    }
+        this.isSold = false;  
 
-                    const unsubscribe = this.store.subscribe(() => {
-                        const { scope, proposalsReady } = this.store.getState();
+        contractStatus({  
+            id: 'contract.purchase_sent',  
+            data: this.tradeOptions.amount,  
+        });  
 
-                        if (scope === BEFORE_PURCHASE && proposalsReady) {
-                            makeDelay()
-                                .then(() => this.observer.emit('REVERT', 'before'))
-                                .finally(() => {
-                                    unsubscribe();
-                                });
-                        }
-                    });
-                },
-                ['PriceMoved', 'InvalidContractProposal'],
-                delayIndex++
-            ).then(onSuccess);
-        }
+        if (!this.options.timeMachineEnabled) {  
+            return doUntilDone(action).then(onSuccess);  
+        }  
 
-        const trade_option = tradeOptionToBuy(
-            contract_type,
-            this.tradeOptions
-        );
-
-        const action = () => api_base.api.send(trade_option);
-
-        this.isSold = false;
-
-        contractStatus({
-            id: 'contract.purchase_sent',
-            data: this.tradeOptions.amount,
-        });
-
-        if (!this.options.timeMachineEnabled) {
-            return doUntilDone(action).then(onSuccess);
-        }
-
-        return recoverFromError(
-            action,
-            (errorCode, makeDelay) => {
-                if (errorCode === 'DisconnectError') {
-                    this.clearProposals();
-                }
-
-                const unsubscribe = this.store.subscribe(() => {
-                    const { scope } = this.store.getState();
-
-                    if (scope === BEFORE_PURCHASE) {
-                        makeDelay()
-                            .then(() => this.observer.emit('REVERT', 'before'))
-                            .finally(() => {
-                                unsubscribe();
-                            });
-                    }
-                });
-            },
-            ['PriceMoved', 'InvalidContractProposal'],
-            delayIndex++
-        ).then(onSuccess);
-    }
-
-    getPurchaseReference = () => purchase_reference;
-
-    regeneratePurchaseReference = () => {
-        purchase_reference = getUUID();
-    };
+        return recoverFromError(  
+            action,  
+            (errorCode, makeDelay) => {  
+                if (errorCode === 'DisconnectError') {  
+                    this.clearProposals();  
+                }  
+                const unsubscribe = this.store.subscribe(() => {  
+                    const { scope } = this.store.getState();  
+                    if (scope === BEFORE_PURCHASE) {  
+                        makeDelay().then(() => this.observer.emit('REVERT', 'before'));  
+                        unsubscribe();  
+                    }  
+                });  
+            },  
+            ['PriceMoved', 'InvalidContractProposal'],  
+            delayIndex++  
+        ).then(onSuccess);  
+    }  
+    getPurchaseReference = () => purchase_reference;  
+    regeneratePurchaseReference = () => {  
+        purchase_reference = getUUID();  
+    };  
 };
