f81d70b06ae1b42421a9e8cf330c35ff364b65c7
peterjohnsngere-cmd <peterjohnsngere@gmail.com>
2026-09-20 12:45:17 +0300

    Update constants.js

--- src/external/bot-skeleton/services/tradeEngine/trade/state/constants.js
+++ src/external/bot-skeleton/services/tradeEngine/trade/state/constants.js
@@ -8,5 +8,4 @@ export const INVALID_ACTION = 'INVALID_ACTION';
 export const SELL = 'SELL';
 export const START = 'START';
 export const STOP = 'STOP';
-export const TRADE_AGAIN = 'TRADE_AGAIN';
 export const NEW_TICK = 'NEW_TICK';
